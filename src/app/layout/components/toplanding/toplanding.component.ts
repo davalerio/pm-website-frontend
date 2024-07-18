823892ac7d14edd8c56ad8633c49df5dd978dd4e
davalerio <danielvb.dev@gmail.com>
2024-07-18 13:58:55 -0500

    update toplanding count agency

--- src/app/layout/components/toplanding/toplanding.component.ts
+++ src/app/layout/components/toplanding/toplanding.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Agency } from 'src/app/class/agency';
+import { AgencyService } from 'src/app/services/agency.service';
 
 @Component({
   selector: 'app-toplanding',
   templateUrl: './toplanding.component.html',
   styleUrls: ['./toplanding.component.scss']
 })
-export class ToplandingComponent {
+export class ToplandingComponent implements OnInit {
+
+  agency: Agency[] = [];
+
+  constructor(private agencyService : AgencyService) {
+
+  }
+
+  ngOnInit(): void {
+    this.agencyService.getAllAgency().subscribe((data) => {
+      this.agency = data;
+    })
+  }
+
 
 }
